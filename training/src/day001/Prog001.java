61d2684e86422fd1dfd6a8e5f91453a32b1b8525
akkinjarapu <akkin@gmail.com>
2022-10-21 22:24:37 +0530

    printing on separate lines

---
+++ training/src/day001/Prog001.java
@@ -0,0 +1,17 @@
+package day001;
+
+public class Prog001 {
+
+	public static void main(String[] args) {
+		/* Java 10 */
+		var a = 100;
+		
+		int b = 200;
+		
+		System.out.println(a + " " + b);
+		
+		System.out.println(a + "\n" + b);
+
+	}
+
+}
