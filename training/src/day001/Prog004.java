330f50320ac9bf3d347fad0429c52ee7e4ca6db1
akkinjarapu <akkin@gmail.com>
2022-10-21 22:40:45 +0530

    same as program3 using printf

---
+++ training/src/day001/Prog004.java
@@ -0,0 +1,18 @@
+package day001;
+
+public class Prog004 {
+
+	public static void main(String[] args) {
+		var a = 100;
+		
+		int b = 200;
+		
+		System.out.printf("%d %d\n", a, b);
+		
+		System.out.printf("%-4d%4d\n", a, b);
+		
+		System.out.printf("%d\n%d", a, b);
+
+	}
+
+}
