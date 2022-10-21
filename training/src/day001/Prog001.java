048ed3580fb3eb6121714502232592c6c28b4ce7
akkinjarapu <akkin@gmail.com>
2022-10-21 22:16:16 +0530

    printing in two lines

--- training/src/day001/Prog001.java
+++ training/src/day001/Prog001.java
@@ -3,10 +3,13 @@ package day001;
 public class Prog001 {
 
 	public static void main(String[] args) {
+		/* var keyword introduced in Java 10 */
 		var a = 100;
 		
+		/* type <identifier> [ = value] */
 		int b = 200;
 		
+		/* two variables printed in two separate lines */
 		System.out.println(a);
 		
 		System.out.println(b);
