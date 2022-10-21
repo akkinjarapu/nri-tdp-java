668291e14b8cc1a8e02d2e6709dc2cecb9285607
akkinjarapu <akkin@gmail.com>
2022-10-21 22:31:42 +0530

    program001 + program002

---
+++ training/src/day001/Prog003.java
@@ -0,0 +1,16 @@
+package day001;
+
+public class Prog003 {
+
+	public static void main(String[] args) {
+		var a = 100;
+		
+		int b = 200;
+		
+		System.out.println(a + " " + b);
+		
+		System.out.println(a + "\n" + b);
+
+	}
+
+}
