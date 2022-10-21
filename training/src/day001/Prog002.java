5876b89e07a5d0926aaff54d2a94bb25aeffd4fe
akkinjarapu <akkin@gmail.com>
2022-10-21 22:28:59 +0530

    printing side by side

---
+++ training/src/day001/Prog002.java
@@ -0,0 +1,18 @@
+package day001;
+
+public class Prog002 {
+
+	public static void main(String[] args) {
+		var a = 100;
+		
+		int b = 200;
+		
+		System.out.print(a);
+		
+		System.out.print(" ");
+		
+		System.out.println(b);
+
+	}
+
+}
